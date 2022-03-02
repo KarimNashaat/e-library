7518ba93f0fafa1a50150602c233605b3576d518
KarimNashaat <karimnashaat97@yahoo.com>
2022-03-02 05:09:05 +0200

    edit backend url

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://kn-e-library-backend.herokuapp.com/ ";
+const baseURL = "https://kn-e-library-backend.herokuapp.com/";
 
 const instance = axios.create({
   // .. congigure axios baseURL
